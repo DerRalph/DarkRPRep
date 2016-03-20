1c641e4300a6b591a0aad9b497d58807c3235a93
DerRalph <nitsch99@outlook.de>
2016-03-20 16:26:14 +0100

    Create sh_sol.lua

---
+++ sh_sol.lua
@@ -0,0 +1,10 @@
+
+FACTION.name = "Soldat"
+FACTION.desc = "Ein normaler Soldat"
+FACTION.isDefault = false
+FACTION.color = (148, 148, 148)
+local SOLDAT_MODELS = {
+}
+FACTION.models =  SOLDAT_MODELS
+FACTION.salary = 150
+FACTION_SOLDAT = FACTION.index
