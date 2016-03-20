9048f8f522dce3fe272b8dc1ff4e0f7b487a87da
DerRalph <nitsch99@outlook.de>
2016-03-20 16:31:43 +0100

    Create 2ndSGT.lua

---
+++ 2ndSGT.lua
@@ -0,0 +1,10 @@
+
+FACTION.name = "2nd Sergeant"
+FACTION.desc = "Ein 2nd Sergeant"
+FACTION.isDefault = false
+FACTION.color = (148, 148, 148)
+local 2ndSGT_MODELS = {
+}
+FACTION.models =  2ndSGT_MODELS
+FACTION.salary = 150
+FACTION_2ndSGT = FACTION.index
