8fc5e7309a77b1d869c5447f9fa65f6af35066bc
DerRalph <nitsch99@outlook.de>
2016-03-20 16:23:57 +0100

    Create sh_srec.lua

---
+++ sh_srec.lua
@@ -0,0 +1,9 @@
+FACTION.name = "Rekrut"
+FACTION.desc = "Der Rekrut der Army"
+FACTION.isDefault = false
+FACTION.color = (148, 148, 148)
+local REKRUT_MODELS = {
+}
+FACTION.models =  REKRUT_MODELS
+FACTION.salary = 150
+FACTION_REKRUT = FACTION.index
